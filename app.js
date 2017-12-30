69b2f5bb0814fa2ba7da73b76723d90dec1f32fc
RavicharanN <ravicharan.vsp@gmail.com>
2017-12-30 22:46:13 +0530

    Add config files

---
+++ app.js
@@ -0,0 +1,11 @@
+require('./config/config.js');
+var env = process.env.NODE_ENV || 'development';
+if(env === 'development') {
+    //Set params for development mode
+}
+else {
+    // Set pararms for production mode
+}
+
+
+
