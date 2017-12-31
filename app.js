f894fc203b8fe56605e64a6997fc44a8cae427f3
RavicharanN <ravicharan.vsp@gmail.com>
2017-12-31 18:45:41 +0530

    Add prompts

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-require('./config/config.js');
-var env = process.env.NODE_ENV || 'development';
-if(env === 'development') {
-    //Set params for development mode
-}
-else {
-    // Set pararms for production mode
-}
+import { REFUSED } from 'dns';
+import { request } from 'https';
+
+var prompt = require('prompt'),
+    config = require('./config/config.js'),
+    csv = require('fast-csv'),
+    fs = require('fs'),
+    stream = fs.createReadStream("test.csv");
+
+// Scan path of csv file
+var path;
+prompt.start();
+console.log('Please enter the path to csv file');
+prompt.get(['csvPath'], function(err,result) {
+
+    path = result.csvPath; //Set the input path to 'path' variable
+    console.log(path);
+
+    // Set parameters according to environment - (Look into REadMe for further details)
+    var env = process.env.NODE_ENV || 'development';
+    if(env === 'development') {
+        //Set params for development mode
+    }
+    else {
+        // Set pararms for production mode
+    }
+    // End of block
+        
+
+})
+
+
 
 
 
